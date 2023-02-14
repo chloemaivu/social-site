b6f140fba2ea668df0ae293d62acf70a980c98fe
Laith Hadi <laithoday5@yahoo.com>
2023-02-14 17:31:59 +0000

    LH: fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
@@ -7,8 +6,7 @@ import './App.css';
 import View from './View'
 import Add from './Add'
 import {
-  HashRouter as Router,
-  Switch,
+  Routes,
   Route,
   Link
 } from "react-router-dom";
@@ -26,12 +24,13 @@ class App extends React.Component {
   componentDidMount() {
     const listContents = localStorage.getItem("posts");
     let postValue = 0
-    if(listContents) {
-      postValue = (JSON.parse(listContents)[JSON.parse(listContents).length -1].postid) 
+    if (listContents) {
+      postValue = (JSON.parse(listContents)[JSON.parse(listContents).length - 1].postid)
     }
-     
+
     this.setState(
-      { posts: JSON.parse(listContents) || [],
+      {
+        posts: JSON.parse(listContents) || [],
         postId: postValue
       }
     )
@@ -48,15 +47,15 @@ class App extends React.Component {
 
     this.setState((state) => ({
       posts: state.posts.map(
-        post => post.postid === id ? { ...post, likes: post.likes +1 } : post
+        post => post.postid === id ? { ...post, likes: post.likes + 1 } : post
       )
     }), () => localStorage.setItem("posts", JSON.stringify(this.state.posts)))
   }
 
   render() {
     return (
-      <Router>
 
+      <>
         <Navbar bg="primary" expand="md">
           <Navbar.Brand>FaceAche™</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -69,7 +68,7 @@ class App extends React.Component {
         </Navbar>
 
         <Container>
-          <Switch>
+          <Routes>
             <Route path="/add">
               <Add onsubmit={(postid, id, text, img, likes) => this.updateListItems(postid, id, text, img, likes)} lastid={this.state.postId} />
             </Route>
@@ -79,12 +78,10 @@ class App extends React.Component {
             <Route path="/">
               Error: 404 not found
             </Route>
-          </Switch>
-
+          </Routes>
         </Container>
 
-      </Router>
-
+      </>
     );
   }
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
+
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
